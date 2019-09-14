8b037d5d5c2e7e7c34e8b9f408bff94ff1e6ce11
IgorFPonchielli <igor.ponchielli@gmail.com>
2019-09-14 15:59:51 -0300

    aplicando routing

--- helpDesk/src/app/app-routing.module.ts
+++ helpDesk/src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AbrirChamadoComponent } from './abrir-chamado/abrir-chamado.component';
 import { ChamadosComponent } from './chamados/chamados.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  { path: 'abrir-chamado', component: AbrirChamadoComponent },
-  { path: 'chamados', component: ChamadosComponent }
+  { path: 'abrir-chamado', component: AbrirChamadoComponent, data: { title: 'Abrir Chamado' } },
+  { path: 'chamados', component: ChamadosComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: '/chamados', pathMatch: 'full' }
 ];
 
 @NgModule({
