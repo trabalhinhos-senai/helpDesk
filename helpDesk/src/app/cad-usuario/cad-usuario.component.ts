ec937d2d011fa6f09f5295734602c309040017c2
IgorFPonchielli <igor.ponchielli@gmail.com>
2019-10-14 21:12:46 -0300

    css global e validação de senha

--- helpDesk/src/app/cad-usuario/cad-usuario.component.ts
+++ helpDesk/src/app/cad-usuario/cad-usuario.component.ts
@@ -14,15 +14,36 @@ export class CadUsuarioComponent implements OnInit {
   public cadastrado = false;
   public confirmarSenha: String;
 
+  //Minimo 6 caracteres, ao menos uma letra e um numero:
+  public padraoNormal: any = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{6,}$";
+
+  //Minimo 8 caracteres, ao menos uma letra, um numero e um caractere especial:
+  public padraoMedio: any = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$";
+
+  //Minimum eight characters, ao menos uma letra maiuscula, uma letra minuscula e um numero:
+  public padraoAlto: any = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$";
+
+  errorMgs: string;
+  selecaoPadrao: string;
+
   constructor(private cadUsuarioService: CadUsuarioService) { }
 
   ngOnInit() {
+    this.selecaoPadrao = this.padraoNormal; //pode alterar entre as tres opções acima
+
+    if (this.selecaoPadrao === this.padraoNormal) {
+      this.errorMgs = 'A senha deve ter no minimo 6 caracteres, ao menos um numero e um caracter.'
+    } else if (this.selecaoPadrao === this.padraoMedio) {
+      this.errorMgs = 'O minimo é 8 caracteres, ao menos uma letra, um numero e um caracter especial.'
+    } else if (this.selecaoPadrao === this.padraoAlto) {
+      this.errorMgs = 'O minimo é 8 caracteres, ao menos uma letra maiuscula, uma minuscula e um numero.'
+    }
 
   }
 
   onSubmit(formulario: NgForm) {
+    
     if (formulario.valid) {
-
 
       if (this.usuario.senha === this.confirmarSenha) {
 
